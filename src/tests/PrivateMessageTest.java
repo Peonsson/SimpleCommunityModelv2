b6de108c6f9bab38e2e1f61c0840b7e5b229b7d9
roppe546 <roppe546@gmail.com>
2015-11-30 16:27:22 +0100

    Added more tests.

---
+++ src/tests/PrivateMessageTest.java
@@ -0,0 +1,63 @@
+import org.junit.Test;
+import org.peonsson.com.BusinessLogic.LogHandler;
+import org.peonsson.com.BusinessLogic.PrivateMessageHandler;
+import org.peonsson.com.Entity.PrivateMessage;
+import org.peonsson.com.ViewModels.*;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by robin on 24/11/15.
+ */
+public class PrivateMessageTest {
+//    @POST
+//    public static ReturnBooleanViewModel submit(PrivateMessageViewModel privateMessage) {
+//        boolean bool = PrivateMessage.submit(privateMessage);
+//        return new ReturnBooleanViewModel(bool);
+//    }
+//
+//    @GET
+//    @Path("/{id}")
+//    public static List<PrivateMessageViewModel> fetchPrivateMessages(@PathParam("id") int id) {
+//        return PrivateMessage.fetchPrivateMessages(id);
+//    }
+
+    @Test
+    public void testCreateNewPMSucceed() throws Exception {
+        PrivateMessageViewModel pm = new PrivateMessageViewModel("1", "2", "TEST SUBJ", "TEST MESS", new Date());
+
+        ReturnBooleanViewModel returnVal = PrivateMessageHandler.submit(pm);
+
+        assert(returnVal.isBool());
+    }
+
+    @Test
+    public void testCreateNewPMFail() throws Exception {
+        PrivateMessageViewModel pm = new PrivateMessageViewModel("-1", "-1", "TEST SUBJ", "TEST MESS", new Date());
+
+        ReturnBooleanViewModel returnVal = PrivateMessageHandler.submit(pm);
+
+        assert(!returnVal.isBool());
+    }
+
+    @Test
+    public void testFetchPMsForUserSucceed() throws Exception {
+        List<PrivateMessageViewModel> pms = PrivateMessageHandler.fetchPrivateMessages(1);
+
+        assert(pms.size() > 0);
+    }
+
+    @Test
+    public void testFetchPMsForUserFail() throws Exception {
+        List<PrivateMessageViewModel> pms = PrivateMessageHandler.fetchPrivateMessages(-1);
+
+        assert(pms.size() == 0);
+    }
+}
